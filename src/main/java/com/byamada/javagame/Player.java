f8131e9f6c3b343a960e2faaaf511d0686c7606c
Bruno Yamada <brunofernandoyamada@gmail.com>
2022-02-02 01:58:03 -0300

    Implementing the Server and Client connections

--- src/main/java/com/byamada/javagame/Player.java
+++ src/main/java/com/byamada/javagame/Player.java
@@ -2,6 +2,10 @@ package com.byamada.javagame;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
 
 public class Player extends JFrame {
     private int width;
@@ -12,6 +16,10 @@ public class Player extends JFrame {
     private JButton b2;
     private JButton b3;
     private JButton b4;
+    private int playerID;
+    private int opponentID;
+
+    private ClientSideConnection clientSideConnection;
 
     public Player(int w, int h) {
         width = w;
@@ -26,7 +34,7 @@ public class Player extends JFrame {
 
     public void setUpGUI() {
         this.setSize(width, height);
-        this.setTitle("Turn-based Game");
+        this.setTitle("Player #"+ playerID);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         contentPane.setLayout(new GridLayout(1, 5));
         contentPane.add(message);
@@ -38,12 +46,53 @@ public class Player extends JFrame {
         contentPane.add(b2);
         contentPane.add(b3);
         contentPane.add(b4);
+
+        if (playerID == 1){
+            message.setText("You are player #1. You go first");
+            opponentID = 2;
+        } else {
+            message.setText("You are player #2. Wait for your turn");
+            opponentID = 1;
+        }
+
         this.setVisible(true);
+    }
+
+    public void connectionToServer() {
+        clientSideConnection = new ClientSideConnection();
     }
 
     public static void main(String[] args) {
         Player p = new Player(500, 100);
+        p.connectionToServer();
         p.setUpGUI();
+    }
+
+    //Client Connection inner class
+    private class ClientSideConnection {
+
+        private Socket socket;
+        private DataInputStream dataInputStream;
+        private DataOutputStream dataOutputStream;
+
+        private final static String HOSTNAME = "localhost";
+        private final static int PORT = 51734;
+
+        public ClientSideConnection() {
+            System.out.println("----Client----");
+            try {
+                socket = new Socket(HOSTNAME, PORT);
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                playerID = dataInputStream.readInt();
+                System.out.println("Connected to server as Player #" + playerID + ".");
+            } catch (IOException ex) {
+                System.out.println("IOException from CSC constructor");
+            }
+        }
+
+
+
     }
 
 
--- src/main/java/com/byamada/javagame/Server.java
+++ src/main/java/com/byamada/javagame/Server.java
@@ -8,6 +8,8 @@ public class Server {
     private ServerSocket serverSocket;
     private int numPlayers;
     private final int PORT = 51734;
+    private ServerSideConnection player1;
+    private ServerSideConnection player2;
 
     public Server() {
         System.out.println("----Game Server----");
@@ -26,7 +28,15 @@ public class Server {
             while (numPlayers < 2) {
                 Socket socket = serverSocket.accept();
                 numPlayers++;
-                System.out.println("Player #" + numPlayers + "has connected");
+                System.out.println("Player #" + numPlayers + " has connected");
+                ServerSideConnection ssc = new ServerSideConnection(socket, numPlayers);
+                if(numPlayers == 1) {
+                    player1 = ssc;
+                } else {
+                    player2 = ssc;
+                }
+                Thread t = new Thread(ssc);
+                t.start();
             }
             System.out.println("We now have 2 players. No longer accepting connections");
 
@@ -38,6 +48,35 @@ public class Server {
     public static void main(String[] args) {
         Server server = new Server();
         server.acceptConnections();
+    }
+
+    private class ServerSideConnection implements Runnable {
+
+        private Socket socket;
+        private DataInputStream dataInputStream;
+        private DataOutputStream dataOutputStream;
+        private int playerID;
+
+        public ServerSideConnection(Socket s, int id) {
+            socket = s;
+            playerID = id;
+            try {
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            } catch (IOException ex) {
+                System.out.println("IOException from SSC constructor");
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                dataOutputStream.writeInt(playerID);
+                dataOutputStream.flush();
+            } catch (IOException ex) {
+                System.out.println("IOException from SSC run() ");
+            }
+        }
     }
 
 }
